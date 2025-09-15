f57f926a00718bd700d7253e44383b10d6426f90
Pepe Valdivia <pepe.valdivia.caballer@gmail.com>
2025-09-14 23:04:51 -0500

    haciendo mas limpio el app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 from main.settings import APP
-from main.urls import register
+
 #from main.middlewares import not_found
 
 if __name__ == '__main__':
-  register(APP)
   #APP.register_error_handler(404, not_found)
   # run app
   APP.run(
--- main/settings.py
+++ main/settings.py
@@ -6,16 +6,18 @@
 from flask import Flask
 from flask_session import Session
 from pathlib import Path
+from .urls import register
 from .middlewares import not_found
 
-# Obtén la ruta absoluta a la raíz del proyecto
+# ruta absoluta a la raíz del proyecto
 BASE_DIR = Path(__file__).resolve().parent.parent  # dos niveles arriba de settings.py
 
+# instancia de la aplicación
 APP = Flask(
   __name__,
-  static_folder=str(BASE_DIR / 'static'),        # ruta absoluta a /static
+  static_folder=str(BASE_DIR / 'static'),
   static_url_path='/',
-  template_folder=str(BASE_DIR / 'templates')   # ruta absoluta a /templates
+  template_folder=str(BASE_DIR / 'templates')
 )
 
 # Configuraciones (tu config aquí)
@@ -26,6 +28,11 @@ APP.config['SESSION_USE_SIGNER'] = True
 APP.config['SESSION_KEY_PREFIX'] = 'session:'
 APP.config['JWT_SECRET_KEY'] = 'tu_clave_secreta_aqui'
 
+# habilitar session
 Session(APP)
 
+# cargar blueprints(rutas)
+register(APP)
+
+# middleware error 404
 APP.register_error_handler(404, not_found)
